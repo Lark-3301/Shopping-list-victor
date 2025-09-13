1d9a6c780ca7bdae221d92338a71aec93b15a748
vicmac-github <69369181+Lark-3301@users.noreply.github.com>
2025-09-13 13:25:44 +0200

    Thisis the first part of the exercise

---
+++ shopping-list.js
@@ -0,0 +1,35 @@
+let shoppingList = []
+
+let firstProduct = {
+    item: "tomates",
+    quantity: 3,
+    purchased: false
+}
+
+shoppingList.push(firstProduct)
+
+let secondProduct = {
+    item: "patatas",
+    quantity: 5,
+    purchased: false
+}
+
+shoppingList.push(secondProduct)
+
+let thirdProduct = {
+    item: "uvas",
+    quantity: 23,
+    purchased: false
+}
+
+shoppingList.push(thirdProduct)
+
+let fourthProduct = {
+    item: "repollo",
+    quantity: 23,
+    purchased: false
+}
+
+shoppingList.push(fourthProduct)
+
+console.table(shoppingList)
